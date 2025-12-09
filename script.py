ba73ebdba81ef732a8e6e43821b21149e8c9cbff
meghakj4 <meghakj4@gmail.com>
2025-12-09 14:24:20 +0530

    Refactor GOOGLE_API_KEY handling in script.py
    Removed check for GOOGLE_API_KEY in environment variables.

--- script.py
+++ script.py
@@ -3,10 +3,7 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 import getpass
 import os
 
-
-if "GOOGLE_API_KEY" not in os.environ:
-    os.environ["GOOGLE_API_KEY"] = ""
-
+# Need API Key in env to work.
 model = ChatGoogleGenerativeAI(
     model="gemini-3-pro-preview",
     temperature=0.01,  # Gemini 3.0+ defaults to 1.0
