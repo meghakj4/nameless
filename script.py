396394eefbbfaf67a986d4a50639241fe6abc34c
meghakj4 <meghakj4@gmail.com>
2025-12-09 14:24:51 +0530

    Removed unused code

--- script.py
+++ script.py
@@ -1,12 +1,5 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 
-import getpass
-import os
-
-
-if "GOOGLE_API_KEY" not in os.environ:
-    os.environ["GOOGLE_API_KEY"] = ""
-
 model = ChatGoogleGenerativeAI(
     model="gemini-3-pro-preview",
     temperature=0.01,  # Gemini 3.0+ defaults to 1.0
