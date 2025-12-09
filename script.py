7abd2048f4d34f60523ab9a4276c23cfd7a5a9cd
meghakj4 <meghakj4@gmail.com>
2025-12-09 14:17:48 +0530

    Initial commit

---
+++ script.py
@@ -0,0 +1,30 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+import getpass
+import os
+
+
+if "GOOGLE_API_KEY" not in os.environ:
+    os.environ["GOOGLE_API_KEY"] = "AIzaSyA9lKqmwFoPT-c0fcbr3Lisl1TaOJG3X28"
+
+model = ChatGoogleGenerativeAI(
+    model="gemini-3-pro-preview",
+    temperature=0.01,  # Gemini 3.0+ defaults to 1.0
+    max_tokens=None,
+    timeout=None,
+    max_retries=2,
+    # other params...
+)
+
+
+
+response = model.invoke("WHen was shakespeare born?")
+print(response.content)
+# print(response.generations[0][0].text)
+
+
+
+####
+# Step 1=> Built simple terminal chatbot with single subject context
+# Step 2 => Add all contexts
+# Final Step => Build the web chatbot (user, if needed => memory)
